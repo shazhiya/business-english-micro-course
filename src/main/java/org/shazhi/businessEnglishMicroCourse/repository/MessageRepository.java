4c2c718f3944b944fa9424e031a4cae8de613b1d
shazhiya <2691032513@qq.com>
2021-03-29 17:09:06 +0800

    page

--- src/main/java/org/shazhi/businessEnglishMicroCourse/repository/MessageRepository.java
+++ src/main/java/org/shazhi/businessEnglishMicroCourse/repository/MessageRepository.java
@@ -2,8 +2,7 @@ package org.shazhi.businessEnglishMicroCourse.repository;
 
 import org.shazhi.businessEnglishMicroCourse.entity.ContactsEntity;
 import org.shazhi.businessEnglishMicroCourse.entity.MessageEntity;
-import org.shazhi.businessEnglishMicroCourse.entity.UserEntity;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
@@ -15,7 +14,7 @@ import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
-public interface MessageRepository extends JpaRepository<MessageEntity,Integer>,JpaSpecificationExecutor<MessageEntity> {
+public interface  MessageRepository extends JpaRepository<MessageEntity,Integer>,JpaSpecificationExecutor<MessageEntity> {
 
     @Query("select con from ContactsEntity con where con.self.userId = :selfId")
     List<ContactsEntity> loadContactors(@Param("selfId") Integer selfId);
@@ -25,7 +24,7 @@ public interface MessageRepository extends JpaRepository<MessageEntity,Integer>,
     Integer markRead(@Param("tid") Integer userId,@Param("sid") Integer userId1,@Param("stime") Timestamp messageSendTime);
 
     @Query("from MessageEntity mess where ((mess.targetUser.userId =:tid and mess.sendUser.userId=:sid) or (mess.targetUser.userId =:sid and mess.sendUser.userId=:tid)) and mess.messageSendTime <= :stime")
-    List<MessageEntity> loadHistory(@Param("tid") Integer userId,@Param("sid") Integer userId1,@Param("stime") Timestamp sendTime, PageRequest page);
+    List<MessageEntity> loadHistory(@Param("tid") Integer userId,@Param("sid") Integer userId1,@Param("stime") Timestamp sendTime, Pageable page);
 
     @Query("from ContactsEntity con where con.self.userId =:sid and con.contactor.userId = :cid")
     ContactsEntity findContacts(@Param("sid") Integer userId,@Param("cid") Integer userId1);
